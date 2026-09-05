b4e243190cb487d36bd139f5d39500c1378e7e25
Hn15 <azzamgantengs123@gmail.com>
2026-09-05 10:50:00 +0800

    bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
-import Navbar from './components/test1';
-import FeatureCard from './components/test0';
-
+//top bar hex #e32d20
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white">
+      <div className="bg-[#ff5959] p-4 text-center font-bold"> </div>
+    </div>
+  );
+}
